5645082307c07a99a5219025b6e540eb9b0be592
Eitan Adler <aaa.ganim@gmail.com>
2021-02-01 19:26:09 +0200

    added chat box and mute key

--- main.js
+++ main.js
@@ -1,116 +1,168 @@
-const contacts = [
-  {
-    id: 1,
-    name: "Neriya, bar-lev",
-    lastMessage:
-      "Hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ",
-    img:
-      "https://cdn1.vectorstock.com/i/1000x1000/38/05/male-face-avatar-logo-template-pictograph-vector-11333805.jpg",
-  },
-  {
-    id: 2,
-    name: "Dana",
-    lastMessage: "hello world",
-    img:
-      "https://cdn5.vectorstock.com/i/1000x1000/06/59/template-avatars-blonde-girl-vector-4930659.jpg",
-  },
-  {
-    id: 3,
-    name: "Gershon",
-    lastMessage: "hello world",
-    img:
-      "https://cdn3.vectorstock.com/i/1000x1000/38/17/male-face-avatar-logo-template-pictograph-vector-11333817.jpg",
-  },
-  {
-    id: 4,
-    name: "Yossi",
-    lastMessage: "hello world",
-    img:
-      "https://cdn2.vectorstock.com/i/1000x1000/38/21/male-face-avatar-logo-template-pictograph-vector-11333821.jpg",
-  },
-  {
-    id: 5,
-    name: "Dani",
-    lastMessage: "hello world",
-    img:
-      "https://cdn2.vectorstock.com/i/1000x1000/38/31/male-face-avatar-logo-template-pictograph-vector-11333831.jpg",
-  },
-  {
-    id: 6,
-    name: "Saba",
-    lastMessage: "hello world",
-    img:
-      "https://cdn1.vectorstock.com/i/1000x1000/37/65/male-face-avatar-logo-template-pictograph-vector-11333765.jpg",
-  },
-  {
-    id: 7,
-    name: "family group",
-    lastMessage: "hello world",
-    img:
-      "https://previews.123rf.com/images/yupiramos/yupiramos1703/yupiramos170316303/74735483-young-people-avatars-group-vector-illustration-design.jpg",
-  },
-];
 const UserInfo = {
   name: "Eitan Adler",
   status: "online",
   img:
     "https://www.creativefabrica.com/wp-content/uploads/2019/12/04/AVATAR_FINAL_2-6-580x386.jpg",
 };
-const ImgContainer = (props) => (
-  <div className="img-container">
-    <img className="contact-img" src={props.img} />
-  </div>
-);
-const TextContainer = (props) => (
-  <div className="text-container">
-    <div className="contact-name">{props.name}</div>
-    <div className="contact-last-message">
-      {props.lastMessage.length > 30
-        ? `${props.lastMessage.slice(0, 30)}...`
-        : props.lastMessage}
-    </div>
-  </div>
-);
-const ContactCard = (props) => (
-  <div className="contact-card">
-    <ImgContainer img={props.img} />
-    <TextContainer name={props.name} lastMessage={props.lastMessage} />
+const ChatBox = ({ contactName }) => (
+  <div className="chat-box-header">
+    {contactName ? (
+      <div>
+        <span>{contactName}</span>
+      </div>
+    ) : (
+      <div>no chat</div>
+    )}
   </div>
 );
 
-const ChatList = () => (
+const ContactCard = ({
+  name,
+  lastMessage,
+  img,
+  ChatBoxNameToChange,
+  muted,
+  toggleMute,
+}) => (
+  <div className="contact-card" onClick={ChatBoxNameToChange}>
+    <img className="contact-img" src={img} />
+
+    <div className="text-container">
+      <div className="contact-name">
+        {name}
+        {muted ? (
+          <span onClick={toggleMute}> unmute</span>
+        ) : (
+          <span onClick={toggleMute}> mute</span>
+        )}
+      </div>
+
+      <div className="contact-last-message">
+        {lastMessage.length > 30
+          ? `${lastMessage.slice(0, 30)}...`
+          : lastMessage}
+      </div>
+    </div>
+  </div>
+);
+
+const ChatList = ({ contacts, ChatBoxNameToChange, muted, toggleMute }) => (
   <div className="chat-list">
-    {contacts.map((contact) => (
+    {contacts.map(({ id, name, lastMessage, img }) => (
       <ContactCard
-        key={contact.id}
-        name={contact.name}
-        lastMessage={contact.lastMessage}
-        img={contact.img}
+        key={id}
+        name={name}
+        lastMessage={lastMessage}
+        img={img}
+        ChatBoxNameToChange={() => ChatBoxNameToChange(name)}
+        muted={muted.includes(id) ? true : false}
+        toggleMute={() => toggleMute(id)}
       />
     ))}
   </div>
 );
-const UserProfileTextContainer = () => (
-  <div className="user-profile-text-container">
-    <div className="user-name">{UserInfo.name}</div>
-    <div className="user-status">{UserInfo.status}</div>
-  </div>
-);
+
 const UserProfile = () => (
   <div className="user-profile">
     <img className="user-img" src={UserInfo.img} />
-    <UserProfileTextContainer />
+    <div className="user-profile-text-container">
+      <div className="user-name">{UserInfo.name}</div>
+      <div className="user-status">{UserInfo.status}</div>
+    </div>
   </div>
 );
-const SideBar = () => (
+const SideBar = ({ contacts, ChatBoxNameToChange, muted, toggleMute }) => (
   <div className="side-bar">
     <UserProfile />
-    <ChatList />;
+    <ChatList
+      contacts={contacts}
+      ChatBoxNameToChange={ChatBoxNameToChange}
+      muted={muted}
+      toggleMute={toggleMute}
+    />
   </div>
 );
-const App = () => (
-  <div className="App">
-    <SideBar />
-  </div>
-);
+class App extends React.Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Neriya, bar-lev",
+        lastMessage:
+          "Hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ",
+        img:
+          "https://cdn1.vectorstock.com/i/1000x1000/38/05/male-face-avatar-logo-template-pictograph-vector-11333805.jpg",
+      },
+      {
+        id: 2,
+        name: "Dana",
+        lastMessage: "hello world",
+        img:
+          "https://cdn5.vectorstock.com/i/1000x1000/06/59/template-avatars-blonde-girl-vector-4930659.jpg",
+      },
+      {
+        id: 3,
+        name: "Gershon",
+        lastMessage: "hello world",
+        img:
+          "https://cdn3.vectorstock.com/i/1000x1000/38/17/male-face-avatar-logo-template-pictograph-vector-11333817.jpg",
+      },
+      {
+        id: 4,
+        name: "Yossi",
+        lastMessage: "hello world",
+        img:
+          "https://cdn2.vectorstock.com/i/1000x1000/38/21/male-face-avatar-logo-template-pictograph-vector-11333821.jpg",
+      },
+      {
+        id: 5,
+        name: "Dani",
+        lastMessage: "hello world",
+        img:
+          "https://cdn2.vectorstock.com/i/1000x1000/38/31/male-face-avatar-logo-template-pictograph-vector-11333831.jpg",
+      },
+      {
+        id: 6,
+        name: "Saba",
+        lastMessage: "hello world",
+        img:
+          "https://cdn1.vectorstock.com/i/1000x1000/37/65/male-face-avatar-logo-template-pictograph-vector-11333765.jpg",
+      },
+      {
+        id: 7,
+        name: "family group",
+        lastMessage: "hello world",
+        img:
+          "https://previews.123rf.com/images/yupiramos/yupiramos1703/yupiramos170316303/74735483-young-people-avatars-group-vector-illustration-design.jpg",
+      },
+    ],
+    contactChatName: null,
+    muted: [],
+  };
+  toggleMute = (id) => {
+    let newArray = [];
+    const oldAraay = this.state.muted;
+    oldAraay.includes(id)
+      ? (newArray = oldAraay.filter((filterID) => filterID !== id))
+      : (newArray = [...oldAraay, id]);
+    return this.setState({ muted: newArray });
+  };
+  render() {
+    const { contacts, contactChatName, muted } = this.state;
+    return (
+      <div className="App">
+        <SideBar
+          contacts={contacts}
+          ChatBoxNameToChange={(name) =>
+            this.setState({ contactChatName: name })
+          }
+          muted={muted}
+          toggleMute={this.toggleMute}
+        />
+        <ChatBox contactName={contactChatName} />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById("app"));
